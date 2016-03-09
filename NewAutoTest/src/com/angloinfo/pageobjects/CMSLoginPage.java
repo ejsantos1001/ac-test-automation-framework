54ab4be20bd7c794c852c2d806b8ed65cf45d048
ej santos <eliazersantos.mail@gmail.com>
2016-03-09 20:07:04 +0800

    Change-Id: I2a728a714eb0cfed69722fd1b244cce1ec603081

---
+++ NewAutoTest/src/com/angloinfo/pageobjects/CMSLoginPage.java
@@ -0,0 +1,37 @@
+package com.angloinfo.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CMSLoginPage {
+	
+	WebDriver driver;
+	WebElement emailfield; 
+	WebElement passwordfield;
+	WebElement loginbutton;
+	
+	public CMSLoginPage(WebDriver driver){
+		this.driver = driver;	
+	}
+	
+	public void goToPage(String baseurl) {
+		this.driver.get(baseurl);
+	}
+	
+	public void typeEmail(String cmsuseremail) {
+		emailfield = driver.findElement(By.id("email"));
+		emailfield.sendKeys(cmsuseremail);
+	}
+	
+	public void typePassword(String cmspassword) {
+		passwordfield = driver.findElement(By.id("password"));
+		passwordfield.sendKeys(cmspassword);
+	}
+	
+	public void clickLogin() {
+		loginbutton = driver.findElement(By.xpath("//button[@type='submit']"));
+		loginbutton.click();
+	}
+	
+}
---
+++ NewAutoTest/src/com/angloinfo/testcases/DirectoryModeration.java
@@ -0,0 +1,40 @@
+package com.angloinfo.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.angloinfo.pageobjects.CMSLoginPage;
+import com.angloinfo.tools.WebDriverManager;
+import com.angloinfo.tools.UrlManager;
+
+
+public class DirectoryModeration {
+	
+	 String febaseurl;
+	 String cmsbaseurl;
+	 String accesstoken;
+	 WebDriver driver;
+	
+	
+	@Parameters({"environment"})
+	@BeforeTest
+	public void beforeTest(String env) {
+		driver = WebDriverManager.getDriver();
+		febaseurl = UrlManager.getFEbaseurl(env);
+		cmsbaseurl =UrlManager.getCMSbaseurl(env);
+	}
+	
+	@Test
+	public void cmsLogin() {
+		
+		CMSLoginPage CMSLoginPage1 = new CMSLoginPage(driver);
+		CMSLoginPage1.goToPage(cmsbaseurl);
+		CMSLoginPage1.typeEmail("dev@angloinfo.com");
+		CMSLoginPage1.typePassword("penguin");
+		CMSLoginPage1.clickLogin();
+	}
+	
+	
+
+}
